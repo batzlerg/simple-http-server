ea5557e255bdb2d6e05fddcfcb370edbacf44f90
graham <batzlerg@gmail.com>
2020-03-14 21:10:48 -0600

    load static files programmatically via request path

--- src/main/java/com/batzlerg/app/App.java
+++ src/main/java/com/batzlerg/app/App.java
@@ -14,39 +14,44 @@ import com.sun.net.httpserver.HttpServer;
 public class App {
   public static void main(String args[]) throws Exception {
     final int PORT_NUMBER = 8000;
-    final String BASE_DIR = "./static/";
+    final String BASE_DIR = "./static/"; // todo: configurable
     HttpServer server = HttpServer.create(new InetSocketAddress(PORT_NUMBER), 0);
 
     server.createContext("/", (HttpExchange exchange) -> {
       URI uri = exchange.getRequestURI();
       String filePath = uri.getPath();
-        System.out.println(System.getProperty("user.dir"));
+      OutputStream os = exchange.getResponseBody();
 
-      if (filePath.contains(".html")) {
-        System.out.println('1');
-        // read file
-        try {
-          String name = new File(uri.getPath()).getName();
-          File file = new File(BASE_DIR, name);
-          System.out.println(name);
-
-          if (file.exists()) {
-            exchange.getResponseHeaders().set("Content-Type", "text/html");
-            exchange.sendResponseHeaders(200, file.length());
-
-            OutputStream os = exchange.getResponseBody();
-            os.write(Files.readAllBytes(file.toPath()));
-            os.close();
-          } else {
-            throw new IOException("file does not exist");
-          }
-        } catch (IOException e) {
-          System.err.println("File not found: " + e);
-          exchange.sendResponseHeaders(404, 0);
+      // todo: regex extension whitelist?
+      // html file or extension-less
+      try {
+        String name = new File(uri.getPath()).getName();
+        // alias "/" to index.html; todo: configurable
+        if (name.length() == 0) {
+          name = "index";
         }
+
+        File file = new File(BASE_DIR, name.concat(".html"));
+
+        if (file.exists()) {
+          exchange.getResponseHeaders().set("Content-Type", "text/html");
+          exchange.sendResponseHeaders(200, file.length());
+
+          os.write(Files.readAllBytes(file.toPath()));
+          os.close();
+        } else {
+          throw new IOException("file does not exist");
+        }
+      } catch (IOException e) {
+        File errPage = new File(BASE_DIR, "404.html"); // todo: configurable
+
+        System.err.println(e);
+
+        exchange.sendResponseHeaders(404, 0);
+
+        os.write(Files.readAllBytes(errPage.toPath()));
+        os.close();
       }
-      // todo: flesh out
-      exchange.sendResponseHeaders(404, 0);
     });
     server.setExecutor(null);
     server.start();
